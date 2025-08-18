2467b5dd268b40ddf04c965bb8369025552e8b34
Komalbaghel08 <baghelkomal512@gmail.com>
2025-08-18 20:16:09 +0530

    my rental car website

---
+++ json.js
@@ -0,0 +1,69 @@
+
+let Book = () => {
+    let name = document.querySelector("#name").value;
+    // let contact = document.querySelector("#contact").value;
+    let contact=document.querySelector("#contactt").value
+    let pickuplocation = document.querySelector("#pickuplocation").value;
+    let dropofflocation = document.querySelector("#dropofflocation").value;
+    let city = document.querySelector("#city").value;
+   
+
+    let url = 'http://localhost:3000/RentGo';
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+           "contact":contact,
+            "pickuplocation":pickuplocation,
+            "dropofflocation":dropofflocation,
+            "city": city
+
+            
+        })
+    })
+
+    location.href = 'index.html';
+    return false;
+}
+
+
+
+// GET
+
+
+ let fatchData = async () => {
+ let url = 'http://localhost:3000/RentGo'
+ 
+ let res = await fetch(url, {method:"GET"})
+
+ let data = await res.json()
+
+ let show = document.querySelector('.showTable1')
+
+ data.map((e)=>{
+    show.innerHTML += `
+        <tr>
+        <td>${e.name}</td>
+        <td>${e.contact}</td>
+        <td>${e.pickuplocation}</td>
+        <td>${e.dropofflocation}</td>
+        <td>${e.city}</td>
+        <td onclick="del('${e.id}')">DELETE</td>
+        </tr>
+    `
+ })
+
+ console.log(data)
+}
+
+fatchData()
+
+let del = (id) =>{
+
+    let url =  `http://localhost:3000/RentGo/${id}`
+    fetch(url, {method:"DELETE"})
+}
---
+++ script.js
@@ -0,0 +1,81 @@
+// Elements
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+// Hamburger toggle for mobile nav
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+  navLinks.classList.toggle("show");
+});
+
+// SignUp Login section
+function openModal(type) {
+  document.getElementById(type + "Modal").style.display = "flex";
+}
+function closeModal(type) {
+  document.getElementById(type + "Modal").style.display = "none";
+}
+
+// Signup Form
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let name = document.getElementById("signupName").value;
+  let email = document.getElementById("signupEmail").value;
+  let contact = document.getElementById("signupContact").value;
+  let password = document.getElementById("signupPassword").value;
+
+  localStorage.setItem(
+    "user",
+    JSON.stringify({ name, email, contact, password })
+  );
+  alert("Signup Successful! Please Login.");
+  closeModal("signup");
+  openModal("login");
+});
+
+// Login Form
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let email = document.getElementById("loginEmail").value;
+  let password = document.getElementById("loginPassword").value;
+  let user = JSON.parse(localStorage.getItem("user"));
+
+  if (user && user.email === email && user.password === password) {
+    alert("Login Successful! Welcome " + user.name);
+    closeModal("login");
+  } else {
+    alert("Invalid email or password!");
+  }
+});
+
+// RIDE SECTION
+// ScrollReveal options
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// Reveal animations for Experience section
+ScrollReveal().reveal(".experience__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+// Reveal animations for Download section
+ScrollReveal().reveal(".download__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".download__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".download__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".download__links", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
